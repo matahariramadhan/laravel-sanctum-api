781593ae1b63b69e49631c001d5fa4c9d14e0e5a
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-08-04 11:03:36 +0200

    correcting protected routes

--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,8 @@ Route::get('products/search/{name}', [ProductController::class, 'search']);
 // Protected Routes
 Route::group(['middleware' => ['auth:sanctum']], function(){
     Route::post('products/', [ProductController::class, 'store']);
-    Route::post('products/', [ProductController::class, 'update']);
-    Route::post('products/', [ProductController::class, 'destroy']);
+    Route::put('products/{id}', [ProductController::class, 'update']);
+    Route::delete('products/{id}', [ProductController::class, 'destroy']);
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
